411f4c9d7b21d7430675b8e4b8eb24917c598fb7
Nikita Glygalo <gnarrik@gmail.com>
2025-12-16 11:24:00 +0700

    Сверстан макет шапки

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,10 +1,26 @@
 import './Header.scss'
+import Logo from "@/shared/ui/Logo";
+import Cart from "@/shared/ui/Cart";
 
 const Header = () => {
 
+  const headerLinks: string[] = ['Репродукции', 'Новинки', 'О нас']
+
   return (
-    <header>
-      Header
+    <header className='header'>
+      <Logo />
+      <nav className='nav'>
+        <ul className='nav__list'>
+          {headerLinks.map((link) => (
+            <li className='nav__item'>
+              <a href="/" className='nav__link-title'>
+                {link}
+              </a>
+            </li>
+          ))}
+          <Cart />
+        </ul>
+      </nav>
     </header>
   )
 }
